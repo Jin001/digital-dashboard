37c11452fa4282895a99a656148a755701cc0ba9
levi@levilynch.com <levi@levilynch.com@414b26fd-436f-b78a-1bcc-543bab57f40f>
2011-11-23 01:43:43 +0000

    Updated Menu.xml strings.xml and abstractdashboard.xml to get the sub menu working on each screen.

--- DigitalDashboard/src/com/dashboard/ui/AbstractDashboardActivity.java
+++ DigitalDashboard/src/com/dashboard/ui/AbstractDashboardActivity.java
@@ -1,6 +1,9 @@
 package com.dashboard.ui;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Abstract class definition for a DashboardActivity; 
@@ -10,6 +13,40 @@ import android.app.Activity;
  */
 public class AbstractDashboardActivity extends Activity {
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		super.onCreateOptionsMenu(menu);
+		
+		// create and set the menu inflater
+		getMenuInflater().inflate(R.menu.menu, menu);
+		
+		// returns true to display the menu
+		return true;
+	}
+
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		super.onOptionsItemSelected(item);
+		
+		switch (item.getItemId())
+		{
+			case R.id.landing:
+				startActivity(new Intent(this, DashboardLandingActivity.class));
+				break;
+			case R.id.main:
+				startActivity(new Intent(this, DashboardDisplayActivity.class));
+				break;
+			case R.id.settings:
+				startActivity(new Intent(this, DashboardSettingsActivity.class));
+				break;
+		}
+		return true;
+	}
+	
+	
+
 
 }
