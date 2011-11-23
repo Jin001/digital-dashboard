57367abc5ea8d7f48b7d83d5dfc6bf5dc08b2f4e
joseph.rains.513 <joseph.rains.513@414b26fd-436f-b78a-1bcc-543bab57f40f>
2011-11-23 15:00:21 +0000

    fixed inheritance of the landing activity and added comments

--- DigitalDashboard/src/com/dashboard/ui/AbstractDashboardActivity.java
+++ DigitalDashboard/src/com/dashboard/ui/AbstractDashboardActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Abstract class definition for a DashboardActivity; 
@@ -14,25 +15,23 @@ import android.view.MenuItem;
 public class AbstractDashboardActivity extends Activity {
 	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
 		super.onCreateOptionsMenu(menu);
 		
 		// create and set the menu inflater
 		getMenuInflater().inflate(R.menu.menu, menu);
 		
-		// returns true to display the menu
 		return true;
 	}
 
 	
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
 		super.onOptionsItemSelected(item);
 		
-		switch (item.getItemId())
-		{
+		switch (item.getItemId()) {
 			case R.id.landing:
 				startActivity(new Intent(this, DashboardLandingActivity.class));
 				break;
@@ -42,6 +41,8 @@ public class AbstractDashboardActivity extends Activity {
 			case R.id.settings:
 				startActivity(new Intent(this, DashboardSettingsActivity.class));
 				break;
+			default:
+				Toast.makeText(this, "Invalid Menu Option", Toast.LENGTH_LONG).show();
 		}
 		return true;
 	}
